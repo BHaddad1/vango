fccec237a23dc2f17a12f2c71395b025fb0d2b58
Blanche Haddad <blancherachel@gmail.com>
2023-02-28 15:10:30 -0700

    feat: Add form to route

--- src/components/App/App/App.js
+++ src/components/App/App/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from "react-router-dom";
 import { fetchVanGoghData } from "../../../APICalls/apiCalls";
 import "./App.css";
 import icon from "../../../assets/logo.jpg"
+import Form from "../../Form/Form";
 
 function App() {
   const [vanGoghData, setVanGoghData] = useState([]);
@@ -27,6 +28,7 @@ function App() {
         <h1 className="header">VanGo</h1>
       </div>
       <Route exact path="/vanGo/" render={() => <Works vanGoghWorks={vanGoghData}/>} />
+      <Route exact path="/vanGo/" render={() => <Form />} />
     </div>
   );
 }
