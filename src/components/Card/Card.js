87ccc8bde1b9f1d141902297a88c8b91ababe97b
Blanche Haddad <blancherachel@gmail.com>
2023-02-28 15:30:14 -0700

    Add BrowserRouter:

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export function Card() {
+export default function Card() {
 
   return (
     <div>
--- src/components/Works/Works.js
+++ src/components/Works/Works.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Card } from "../Card/Card";
 
-export function Works() {
+export default function Works() {
 
 
 
