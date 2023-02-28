402078ab460e0536894a152ed38611b9b32a7242
Blanche Haddad <blancherachel@gmail.com>
2023-02-28 16:03:53 -0700

    feat: Change Ids for cards

--- src/components/Works/Works.js
+++ src/components/Works/Works.js
@@ -1,13 +1,23 @@
 import React from "react";
 import { Card } from "../Card/Card";
 
-export default function Works() {
+export default function Works({ vanGoghWorks }) {
 
-
+const mappedVanGoghWorks = vanGoghWorks.map(work => {
+  return (
+    <div key={work.objectID}>
+      <Card 
+        id={work.objectID}
+        image={work.primaryImage}
+        title={work.title}
+      />
+    </div>
+    )
+  })
 
   return (
     <div className="card-container">
-
+     {mappedVanGoghWorks}
     </div>
   )
 }
