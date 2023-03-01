bf3cdc1cd75fda0de3bb8ada9c59169c4e8bd861
Blanche Haddad <blancherachel@gmail.com>
2023-03-01 08:36:18 -0700

    feat: Add information to display on CardDetails view

--- src/components/CardDetails/CardDetails.js
+++ src/components/CardDetails/CardDetails.js
@@ -1,10 +1,19 @@
 import React from "react";
 
-export default function CardDetails() {
+export default function CardDetails({ workData }) {
 
+  console.log(workData);
   return (
     <div>
-      <h2>Hello</h2>
+      <img src={workData.primaryImage} alt={workData.title} className="primary-img"/>
+      <h2 className="title-details">Title: {workData.title} by {workData.artistDisplayName}</h2>
+      <p className="work-details">Gallery Number: {workData.GalleryNumber}</p>
+      <p className="work-details">Accession Year: {workData.accessionYear}</p>
+      <p className="work-details">Dimensions: {workData.dimensions}</p>
+      <p className="work-details">Medium: {workData.medium}</p>
+      <p className="work-details">Department: {workData.department}</p>
+      <p className="work-details">More information: {workData.objectURL}</p>
+      <p className="work-details">{workData.objectWikiData_URL && `More information: ${workData.objectWikiData_URL}`}</p>
     </div>
   )
 }
