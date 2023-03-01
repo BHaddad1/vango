5ea019923f0b1a087ff39c64d6b2a7c074a5dd2d
Blanche Haddad <blancherachel@gmail.com>
2023-03-01 13:55:34 -0700

    feat: Add functionality to favorite a work

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, Fragment } from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Link } from "react-router-dom";
 import { fetchVanGoghData } from "../../APICalls/apiCalls";
 import "./App.css";
 import icon from "../../assets/logo.jpg";
@@ -7,15 +7,21 @@ import Form from "../Form/Form";
 import Works from "../Works/Works";
 import CardDetails from "../CardDetails/CardDetails";
 import loadingGif from "../../assets/loadingGif.webp";
+import { Favorites } from "../Favorites/Favorites";
 
 function App() {
   const [vanGoghData, setVanGoghData] = useState([]);
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(true);
+  const [favoritedWorks, setFavoritedWorks] = useState([]);
 
   const getVanGoghWorks = () => {
     fetchVanGoghData()
       .then((data) => {
+        data.map(work => {
+          work.isFavorited = false;
+          return work;
+        })
         setVanGoghData(data);
         setLoading(false);
       })
@@ -27,11 +33,25 @@ function App() {
   }, []);
   console.log(vanGoghData);
 
+  const updateFavorite = (id) => {
+    const foundWork = vanGoghData.map(work => {
+      if (work.objectID === id) {
+       return {...work, isFavorited: !work.isFavorited}
+      } return work;
+    });
+    const favorite = foundWork.find(work => work.objectID === id)
+    console.log("favoritedWork", favorite);
+    setFavoritedWorks([...favoritedWorks, favorite]);
+  }
+
   return (
     <div>
       <div className="header-container">
         <img src={icon} alt="Van Gogh Sunflowers" className="logo" />
         <h1 className="header">VanGo</h1>
+        <Link to="/favorites">
+          <button>Favorites</button>
+        </Link>
       </div>
       <Switch>
         <Route exact path="/" render={() => (
@@ -43,9 +63,10 @@ function App() {
           </Fragment>
           )}
         />
+        <Route exact path="/favorites" render={() => <Favorites workData={favoritedWorks}/> }/>
         <Route exact path="/:workId" render={({ match }) => {
           const foundWork = vanGoghData.find(work => work.objectID === +match.params.workId)
-            return <CardDetails workID={match.params.workId} workData={foundWork} />;
+            return <CardDetails workID={match.params.workId} workData={foundWork} updateFavorite={updateFavorite} />;
           }}
         />
       </Switch>
