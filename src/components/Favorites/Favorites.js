2d0ab3220db266a00fce25ee472b1fdb6f39c403
Blanche Haddad <blancherachel@gmail.com>
2023-03-01 14:34:57 -0700

    feat: Add back button for home

--- src/components/Favorites/Favorites.js
+++ src/components/Favorites/Favorites.js
@@ -1,10 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Works from "../Works/Works";
 
 export function Favorites({ workData }) {
   console.log(workData);
   return (
     <div>
+      <div className="button-container">
+        <Link to="/">
+          <button className="back-button">Back Home</button>
+        </Link>
+      </div>
       <Works vanGoghWorks={workData} />
     </div>
   )
