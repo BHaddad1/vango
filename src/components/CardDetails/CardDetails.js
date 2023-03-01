48a5e6540c71226cd033646a1e451c3ed0deef9e
Blanche Haddad <blancherachel@gmail.com>
2023-03-01 13:55:53 -0700

    feat: Add a checkbox to favorite a work

--- src/components/CardDetails/CardDetails.js
+++ src/components/CardDetails/CardDetails.js
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
-export default function CardDetails({ workData }) {
+export default function CardDetails({ workData, updateFavorite }) {
+
+  const [favorite, setFavorite] = useState(false);
+
+  useEffect(() => {
+    updateFavorite(workData.objectID);
+  }, [favorite])
 
   console.log(workData);
+
   return (
     <div>
       <div className="button-container">
         <Link to="/">
           <button className="back-button">Back Home</button>
         </Link>
+      </div>
+      <div>
+        <label>
+          <input 
+            type="checkbox"
+            name="favorite"
+            checked={favorite}
+            onChange={event => setFavorite(event.target.checked)}
+          />Favorite this work
+        </label>
       </div>
       <img src={workData.primaryImage} alt={workData.title} className="primary-img"/>
       <h2 className="title-details">{workData.title} by {workData.artistDisplayName}</h2>
