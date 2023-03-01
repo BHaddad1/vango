ff916843ff8979833346b9c4363050f3b77682e6
Blanche Haddad <blancherachel@gmail.com>
2023-03-01 09:13:30 -0700

    feat: Add more details to display

--- src/components/CardDetails/CardDetails.js
+++ src/components/CardDetails/CardDetails.js
@@ -11,13 +11,14 @@ export default function CardDetails({ workData }) {
           <button className="back-button">Back Home</button>
         </Link>
       </div>
-      <img src={workData.primaryImageSmall} alt={workData.title} className="primary-img"/>
+      <img src={workData.primaryImage} alt={workData.title} className="primary-img"/>
       <h2 className="title-details">{workData.title} by {workData.artistDisplayName}</h2>
       <p className="work-details">Gallery Number: {workData.GalleryNumber}</p>
       <p className="work-details">Accession Year: {workData.accessionYear}</p>
       <p className="work-details">Dimensions: {workData.dimensions}</p>
       <p className="work-details">Medium: {workData.medium}</p>
       <p className="work-details">Department: {workData.department}</p>
+      <p className="work-details">Credit Line: {workData.creditLine}</p>
       <p className="work-details">More information: {workData.objectURL}</p>
       <p className="work-details">{workData.objectWikiData_URL && `More information: ${workData.objectWikiData_URL}`}</p>
     </div>
