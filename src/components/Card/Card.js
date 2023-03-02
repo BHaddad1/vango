52415285b6c4f35ee39dd534eccbbabae203a865
Blanche Haddad <blancherachel@gmail.com>
2023-03-02 08:58:34 -0700

    fix: Add conditional rendering for favoriting/unfavoriting a work

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,11 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
-export function Card({ id, image, title }) {
+
+export function Card({ id, image, title, updateFavorite }) {
+
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleOnChange = () => {
+    setIsChecked(!isChecked)
+    updateFavorite(id, isChecked)
+  }
 
   return (
     <div className="card">
-      <img src={image} alt="A Van Gogh work" className="card-image"/>
-      <p className="card-title">{title}</p>
+       {!isChecked && <div>
+        <label>
+          <input
+            type="checkbox"
+            name="favorite"
+            checked={isChecked}
+            onChange={handleOnChange}
+          />
+          Add to Favorites
+        </label>
+      </div>}
+      {isChecked && <div>
+        <label>
+          <input
+            type="checkbox"
+            name="favorite"
+            checked={isChecked}
+            onChange={handleOnChange}
+          />
+          Remove from Favorites
+        </label>
+      </div>}
+      <Link to={`/${id}`}>
+        <img src={image} alt="A Van Gogh work" className="card-image"/>
+        <p className="card-title">{title}</p>
+      </Link>
     </div>
   )
 }
