f90199d688a2386748d3e15d7eaecb501422a055
Blanche Haddad <blancherachel@gmail.com>
2023-03-03 10:43:47 -0700

    feat: Add Link to home page

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -1,8 +1,14 @@
+import "./Error.css";
+import { Link } from "react-router-dom";
+
 export default function Error() {
 
   return (
-    <div>
-      <h2>Page not found.</h2>
-    </div>
+    <section className="error-container">
+      <h2>Sorry, page not found.</h2>
+      <Link to="/">
+        <button className="error-button">Go Back Home</button>
+      </Link>
+    </section>
   )
 }
