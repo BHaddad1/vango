1becbcab09672d4875f381d944b5ba0c71733143
Blanche Haddad <blancherachel@gmail.com>
2023-03-03 15:14:15 -0700

    feat: Save work from main branch

--- src/components/CardDetails/CardDetails.js
+++ src/components/CardDetails/CardDetails.js
@@ -13,13 +13,8 @@ export default function CardDetails({ workData, updateFavorite }) {
   }
 
   return (
-<<<<<<< HEAD
-    <div>
-      <div className="button-container-single">
-=======
     <div className="details">
       <div className="button-container-details"> 
->>>>>>> e66dcbb2464237281d9a082fde4625f1ab30f233
         <Link to="/">
           <button className="back-button-single">Back Home</button>
         </Link>
@@ -62,44 +57,6 @@ export default function CardDetails({ workData, updateFavorite }) {
         <p className="work-details">More information: {workData.objectURL}</p>
         <p className="work-details">{workData.objectWikiData_URL && `More information: ${workData.objectWikiData_URL}`}</p>
       </div>
-<<<<<<< HEAD
-      <div className="details-container">
-        {!isChecked && <div>
-          <label className="favorite-details">
-            <input
-              type="checkbox"
-              name="favorite"
-              checked={isChecked}
-              onChange={handleOnChange}
-              className="favorite-details"
-            />
-            Add to Favorites
-          </label>
-        </div>}
-        {isChecked && <div>
-          <label className="favorite-details">
-            <input
-              type="checkbox"
-              name="favorite"
-              checked={isChecked}
-              onChange={handleOnChange}
-            />
-            Remove from Favorites
-          </label>
-        </div>}
-        <img src={workData.primaryImageSmall} alt={workData.title} className="primary-img"/>
-        <h2 className="title-details">{workData.title} by {workData.artistDisplayName}</h2>
-        <p className="work-details">Gallery Number: {workData.GalleryNumber}</p>
-        <p className="work-details">Accession Year: {workData.accessionYear}</p>
-        <p className="work-details">Dimensions: {workData.dimensions}</p>
-        <p className="work-details">Medium: {workData.medium}</p>
-        <p className="work-details">Department: {workData.department}</p>
-        <p className="work-details">Credit Line: {workData.creditLine}</p>
-        <p className="work-details">More information: {workData.objectURL}</p>
-        <p className="work-details">{workData.objectWikiData_URL && `More information: ${workData.objectWikiData_URL}`}</p>
-      </div>
-=======
->>>>>>> e66dcbb2464237281d9a082fde4625f1ab30f233
     </div>
   )
 }
